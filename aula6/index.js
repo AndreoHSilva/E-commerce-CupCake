eb270ee9e131c2945596b231359a23f86159d130
Andreo Silva <andreohsilva_@hotmail.com>
2024-03-17 21:35:36 -0300

    Adicionado informações na aula 6 sobre variáveis.

--- aula6/index.js
+++ aula6/index.js
@@ -5,3 +5,11 @@ console.log('Em 2000', nome, 'conheceu Maria.');
 console.log(nome, 'casou-se com Maria em 2012.');
 console.log('Maria teve 1 filho com', nome, 'em 2015.');
 console.log('O filho de', nome, 'se chama Eduardo.');
+/*
+Não podemos criar variáveis com palavras reservadas
+Variáveis precisam ter nomes significativos
+Não pode começar o nome de uma variável com um número
+Não podem conter espaços ou traços
+Utilizamos camelCase
+case-sensitive
+*/
